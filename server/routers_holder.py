38c9f6fb77aeb702326faf40a2adfb7d8fd6255f
Daottt <65667345+Daottt@users.noreply.github.com>
2023-12-15 13:30:07 +0300

    Delete server directory

--- server/routers_holder.py
+++
@@ -1,9 +0,0 @@
-from .router import Router
-from .resolver import Resolver
-from .routers_resolvers.auth_router import AuthRouter
-from src.database.models import *
-
-UserRouter = AuthRouter[Users]("Users", Resolver[Users]())
-RequestsRouter = Router[Requests]("Requests", Resolver[Requests]())
-
-routers = (UserRouter.router, RequestsRouter.router)
--- server/routers_resolvers/auth_resolver.py
+++
@@ -1,9 +0,0 @@
-from src.database.db_manager import db
-
-def login(name: str, password: str):
-
-    res = db.execute(
-        query=f'select * from Users where login=(?) and password =(?)',
-        args=(name, password)
-    )
-    return res
